26be6d1f7cd64e77d2d06cf7c0695987c67a371b
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-05-01 15:14:34 +0530

    first commit

---
+++ src/Componets/Helper.js
@@ -0,0 +1,79 @@
+import thefirm from "../assets/images/the-firm.png";
+import dompong from "../assets/images/dompong.png";
+import exculsive from "../assets/images/exculsive-games.png";
+import compnay from "../assets/images/compnay.png";
+import noblemind1 from "../assets/images/noble-mind1.png";
+import noblemind2 from "../assets/images/noble-mind2.png";
+import evergreen from "../assets/images/ever-green.png";
+import julian from "../assets/images/julian.png";
+import ftplane from '../assets/images/ft-plane.png'
+import hubrich from '../assets/images/hubrich.png'
+
+import labs from "../assets/images/games.png";
+export const jan = [
+  {
+    img: thefirm,
+    gitlink1: "https://github.com/INDERJEETBHADU/THE-FIRM.git",
+    livelink1: "https://the-firms.netlify.app/",
+  },
+
+  {
+    img: dompong,
+    gitlink1: "https://github.com/Vishalmanglaw453/Dompkong.git",
+    livelink1: "https://guileless-horse-f710c8.netlify.app/",
+  },
+  {
+    img: ftplane,
+    gitlink1: "https://github.com/Vishalmanglaw453/FTPlane.git",
+    livelink1: "https://vishal-ftplanes.netlify.app/",
+  },
+];  
+export const feb = [
+  {
+    img: exculsive,
+    gitlink1: "https://github.com/Vishalmanglaw453/Exclusive-Games.git",
+    livelink1: "https://exclusive-games-five.vercel.app/",
+  },
+
+  {
+    img: compnay,
+    gitlink1: "https://github.com/Vishalmanglaw453/digital-agency.git",
+    livelink1: "https://digital-agency-sable-kappa.vercel.app/",
+  },
+  {
+    img: noblemind1,
+    gitlink1: "https://github.com/Vishalmanglaw453/noblemind.git",
+    livelink1: "https://noblemind-vishal-manglaws-projects.vercel.app/",
+  },
+ 
+  {
+    img: evergreen,
+    gitlink1: "https://github.com/Vishalmanglaw453/EVER-GREEN.git ",
+    livelink1: "https://ever-green-two.vercel.app/",
+  },
+];
+export const march = [
+  {
+    img: julian,
+    gitlink1: "https://github.com/Vishalmanglaw453/THE-JULIAN-PROJECT.git",
+    livelink1: "https://the-julian-project-six.vercel.app/",
+  },
+  {
+    img: labs,
+    gitlink1: "https://github.com/Vishalmanglaw453/248_labs.git",
+    livelink1: "https://248-labs-jgug.vercel.app/",
+  },
+];
+export const april = [
+  {
+    img: noblemind2,
+    gitlink1: "https://github.com/Vishalmanglaw453/noble_mind.git",
+    livelink1: "https://noble-mind-v87u.vercel.app/",
+  },
+  {
+    img: hubrich,
+    gitlink1: "https://github.com/Vishalmanglaw453/Hubrich-1.git",
+    livelink1: "https://hubrich-1-w5nv.vercel.app/",
+  },
+
+];
---
+++ src/Componets/Worksheet.jsx
@@ -0,0 +1,215 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import thefirm from "../assets/images/the-firm.png";
+import dompong from "../assets/images/dompong.png";
+import exculsive from "../assets/images/exculsive-games.png";
+import compnay from "../assets/images/compnay.png";
+import noblemind1 from "../assets/images/noble-mind1.png";
+import noblemind2 from "../assets/images/noble-mind2.png";
+import evergreen from "../assets/images/ever-green.png";
+import julian from "../assets/images/julian.png";
+
+import labs from "../assets/images/games.png";
+import { jan } from "./Helper";
+import { feb } from "./Helper";
+import { march } from "./Helper";
+import { april } from "./Helper";
+
+const Worksheet = () => {
+  const [ActiveKey, setActiveKey] = useState("nav1");
+  useEffect(() => {
+    setActiveKey("nav1");
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <h1 className=" text-center my-5">WORKSHEET - 2024</h1>
+
+        <p className=" text-uppercase text-end">vishal manglaw</p>
+        <p className=" text-uppercase text-end">id :- 453</p>
+
+        <div className="row">
+          <div className="col-lg-3 col-12">
+            <div className=" d-flex flex-lg-column  flex-row   overflow_scroll gap-4">
+              <button
+                className={`${
+                  ActiveKey === "nav1"
+                    ? "bg-black text-white "
+                    : "bg-transparent"
+                }  nav_btn  `}
+                onClick={() => setActiveKey("nav1")}
+              >
+                January
+              </button>
+              <button
+                className={`${
+                  ActiveKey === "nav2"
+                    ? " bg-black text-white "
+                    : "bg-transparent"
+                }    nav_btn `}
+                onClick={() => setActiveKey("nav2")}
+              >
+                february
+              </button>
+              <button
+                className={`${
+                  ActiveKey === "nav3"
+                    ? " bg-black text-white "
+                    : "bg-transparent"
+                }    nav_btn `}
+                onClick={() => setActiveKey("nav3")}
+              >
+                March
+              </button>
+              <button
+                className={`${
+                  ActiveKey === "nav4"
+                    ? " bg-black text-white "
+                    : "bg-transparent"
+                }    nav_btn `}
+                onClick={() => setActiveKey("nav4")}
+              >
+                April
+              </button>
+            </div>
+          </div>
+          <div className="col-lg-9 col-12">
+            <div
+              className={`${
+                ActiveKey === "nav1" ? "  d-block    " : "  d_none"
+              }  `}
+            >
+              {jan.map((data, index) => (
+                <>
+                  <div className="row">
+                    <div className="col-sm-4 mt-3">
+                      <img className=" w-100" src={data.img} alt="" />
+                    </div>
+                    <div className="col-sm-8 mt-4">
+                      <h2 className=" mb-0">
+                        Github-link -:
+                        <a target="blank" href={data.gitlink1}>
+                          {data.gitlink1}
+                        </a>
+                      </h2>
+                      <p className=" mb-0 pt-3">
+                        Live-link-:
+                        <a
+                          target="blank"
+                          href="  https://the-firms.netlify.app/"
+                        >
+                          {data.livelink1}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+
+            <div
+              className={`${
+                ActiveKey === "nav2" ? "  d-block    " : "  d_none"
+              }  `}
+            >
+              {feb.map((data, index) => (
+                <>
+                  <div className="row">
+                    <div className="col-sm-4 mt-3">
+                      <img className=" w-100" src={data.img} alt="" />
+                    </div>
+                    <div className="col-sm-8 mt-4">
+                      <h2 className=" mb-0">
+                        Github-link -:
+                        <a target="blank" href={data.gitlink1}>
+                          {data.gitlink1}
+                        </a>
+                      </h2>
+                      <p className=" mb-0 pt-3">
+                        Live-link-:
+                        <a
+                          target="blank"
+                          href="  https://the-firms.netlify.app/"
+                        >
+                          {data.livelink1}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+            <div
+              className={`${
+                ActiveKey === "nav3" ? "  d-block    " : "  d_none"
+              }  `}
+            >
+              {march.map((data, index) => (
+                <>
+                  <div className="row">
+                    <div className="col-sm-4 mt-3">
+                      <img className=" w-100" src={data.img} alt="" />
+                    </div>
+                    <div className="col-sm-8 mt-4">
+                      <h2 className=" mb-0">
+                        Github-link -:
+                        <a target="blank" href={data.gitlink1}>
+                          {data.gitlink1}
+                        </a>
+                      </h2>
+                      <p className=" mb-0 pt-3">
+                        Live-link-:
+                        <a
+                          target="blank"
+                          href="  https://the-firms.netlify.app/"
+                        >
+                          {data.livelink1}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+            <div
+              className={`${
+                ActiveKey === "nav4" ? "  d-block    " : "  d_none"
+              }  `}
+            >
+              {april.map((data, index) => (
+                <>
+                  <div className="row ">
+                    <div className="col-sm-4 mt-3">
+                      <img className=" w-100" src={data.img} alt="" />
+                    </div>
+                    <div className="col-sm-8 mt-4">
+                      <h2 className=" mb-0">
+                        Github-link -:
+                        <a target="blank" href={data.gitlink1}>
+                          {data.gitlink1}
+                        </a>
+                      </h2>
+                      <p className=" mb-0 pt-3">
+                        Live-link-:
+                        <a
+                          target="blank"
+                          href="  https://the-firms.netlify.app/"
+                        >
+                          {data.livelink1}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Worksheet;
